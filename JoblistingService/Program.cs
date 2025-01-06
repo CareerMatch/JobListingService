796d7caa40d8da6da06981f8a100c4cb6ce032fb
Bagus Nugra <bagusnugrahaxd@gmail.com>
2025-01-06 15:39:13 +0100

    Add Unit Test and Caching

--- JoblistingService/Program.cs
+++ JoblistingService/Program.cs
@@ -1,17 +1,27 @@
 using JoblistingService.Config;
 using JoblistingService.Repositories;
 using JoblistingService.Services;
+using Prometheus;
+using Microsoft.Extensions.Caching.Distributed;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure MongoDB settings
-
 builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDbSettings"));
 builder.Services.AddSingleton<MongoDbContext>();
+
 // Register services and repositories
 builder.Services.AddScoped<IJobListingRepository, JobListingRepository>();
 builder.Services.AddScoped<IJobListingService, JobListingService>();
 
+// Add Redis Caching
+var redisConnectionString = builder.Configuration.GetValue<string>("Redis:ConnectionString"); // e.g., "localhost:6379"
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = redisConnectionString;
+    options.InstanceName = "JobListingService:";
+});
 
 // Add controllers
 builder.Services.AddControllers()
@@ -19,6 +29,7 @@ builder.Services.AddControllers()
     {
         options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
     });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -30,6 +41,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMetricServer(); // Default: /metrics
+app.UseHttpMetrics(); 
 
 app.UseRouting();
 app.UseAuthorization();
